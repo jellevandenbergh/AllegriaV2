004d56107abd57b8470153dd523d0738b5959637
Bart den Hoed <bartdhoed@gmail.com>
2017-01-24 11:25:51 +0100

    Fix bug url paths

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,37 +5,37 @@
     <!-- META -->
     <meta charset="utf-8">
     <!-- CSS -->
-    <link rel="stylesheet" href="css/style.css" />
-    <link rel="stylesheet" href="css/custom.css" />
-    <link rel="icon" href="images/icon.png">
+    <link rel="stylesheet" href="<?= url('/'); ?>/css/style.css" />
+    <link rel="stylesheet" href="<?= url('/'); ?>/css/custom.css" />
+    <link rel="icon" href="<?= url('/'); ?>/images/icon.png">
     <!-- JS -->
     <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
 </head>
 <body>
     <div class="header2">
         <div class="navigation2">
-            <a href="home" class="logo2">Allegria - Personeelsvereniging Da Vinci College</a>
+            <a href="<?= url('/'); ?>/home" class="logo2">Allegria - Personeelsvereniging Da Vinci College</a>
             <ul class="navigation2left">
             @if($user = Auth::guest())
                 <li>
-                    <a href="home">Home</a>
+                    <a href="<?= url('/'); ?>/home">Home</a>
                 </li>
             @elseif($user = Auth::user()->user_account_type <= 2)
                 <li>
-                    <a href="account">Account</a>
+                    <a href="<?= url('/'); ?>/account">Account</a>
                 </li>
                 <li>
-                    <a href="activities">Activiteiten</a>
+                    <a href="<?= url('/'); ?>/activities">Activiteiten</a>
                 </li>
             @elseif($user = Auth::user()->user_account_type >= 3)
                 <li>
-                    <a href="account">Account</a>
+                    <a href="<?= url('/'); ?>/account">Account</a>
                 </li>
                 <li>
-                    <a href="activities">Activiteiten</a>
+                    <a href="<?= url('/'); ?>/activities">Activiteiten</a>
                 </li>
                 <li>
-                    <a href="members">Leden</a>
+                    <a href="<?= url('/'); ?>/members">Leden</a>
                 </li>
             @endif
             </ul>
