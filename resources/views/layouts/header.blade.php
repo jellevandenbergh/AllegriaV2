793df6b5aad17b7b0b1de72dbb1fc90646fcd4ec
Bart den Hoed <bartdhoed@gmail.com>
2017-01-31 09:47:06 +0100

    fix url

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,37 +5,37 @@
     <!-- META -->
     <meta charset="utf-8">
     <!-- CSS -->
-    <link rel="stylesheet" href="http://localhost/AllegriaV2/public/css/style.css" />
-    <link rel="stylesheet" href="http://localhost/AllegriaV2/public/css/custom.css" />
-    <link rel="icon" href="http://localhost/AllegriaV2/public/images/icon.png">
+    <link rel="stylesheet" href="<?= url('/'); ?>/css/style.css" />
+    <link rel="stylesheet" href="<?= url('/'); ?>/css/custom.css" />
+    <link rel="icon" href="<?= url('/'); ?>/images/icon.png">
     <!-- JS -->
     <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
 </head>
 <body>
     <div class="header2">
         <div class="navigation2">
-            <a href="http://localhost/AllegriaV2/public/home" class="logo2">Allegria - Personeelsvereniging Da Vinci College</a>
+            <a href="<?= url('/'); ?>/home" class="logo2">Allegria - Personeelsvereniging Da Vinci College</a>
             <ul class="navigation2left">
             @if($user = Auth::guest())
                 <li>
-                    <a href="http://localhost/AllegriaV2/public/home">Home</a>
-                </li> 
+                    <a href="<?= url('/'); ?>/home">Home</a>
+                </li>
             @elseif($user = Auth::user()->user_account_type <= 2)
                 <li>
-                    <a href="http://localhost/AllegriaV2/public/account">Account</a>
+                    <a href="<?= url('/'); ?>/account">Account</a>
                 </li>
                 <li>
-                    <a href="http://localhost/AllegriaV2/public/activities">Activiteiten</a>
+                    <a href="<?= url('/'); ?>/activities">Activiteiten</a>
                 </li>
             @elseif($user = Auth::user()->user_account_type >= 3)
                 <li>
-                    <a href="http://localhost/AllegriaV2/public/account">Account</a>
+                    <a href="<?= url('/'); ?>/account">Account</a>
                 </li>
                 <li>
-                    <a href="http://localhost/AllegriaV2/public/activities">Activiteiten</a>
+                    <a href="<?= url('/'); ?>/activities">Activiteiten</a>
                 </li>
                 <li>
-                    <a href="http://localhost/AllegriaV2/public/members">Leden</a>
+                    <a href="<?= url('/'); ?>/members">Leden</a>
                 </li>
             @endif
             </ul>
@@ -60,7 +60,7 @@
                 <li>
                     <a href="http://localhost/AllegriaV2/public/logout">Uitloggen</a>
                 </li>
-            
+
             @endif
             </ul>
         </div>
